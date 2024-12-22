7834b923976167e23bea3c04ae1ca0c65868a14e
Rob Ostheimer <robostheimer@yahoo.com>
2024-12-21 22:05:16 -0500

    removing cheerio dependency

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -140,28 +140,3 @@ const extractItems = async (page) => {
 
   return events_results;
 }
-
-const getProxy = () => {
-  let ip_addresses = [];
-  let port_numbers = [];
-  let random_number = Math.floor(Math.random() * 100);
-  request("https://sslproxies.org/", function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-
-      $("td:nth-child(1)").each(function(index, value) {
-        ip_addresses[index] = $(this).text();
-      });
-
-      $("td:nth-child(2)").each(function(index, value) {
-        port_numbers[index] = $(this).text();
-      });
-    } else {
-      console.log("Error loading proxy, please try again");
-    }
-
-
-
-    return { address: ip_addresses[random_number],  port: port_numbers[random_number]};
-});
-}
