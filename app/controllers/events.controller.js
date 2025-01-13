8dc28e8b631fd3d858dd92f1bb8f1d1901808c30
Rob Ostheimer <robostheimer@yahoo.com>
2025-01-13 09:48:47 -0500

    going back to 3000 timeout

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -112,14 +112,14 @@ exports.findAll = async (req, res) => {
 }
 
 const extractItems = async (page, pagination, retries = 3) => {
-  await page.waitForSelector('li.small-city', {timeout: 1500  });
+  await page.waitForSelector('li.small-city', {timeout: 3000  });
   let href = await page.evaluate(() => {
     if(!document.querySelector("li.small-city")){
       return ['undefined'];
     } 
     return document.querySelector("li.small-city a").getAttribute('href');
   })
-
+  console.log(href)
   const today = new Date();
   const twoWeeksFromToday = new Date();
   twoWeeksFromToday.setDate(today.getDate() + 14);
@@ -129,7 +129,7 @@ const extractItems = async (page, pagination, retries = 3) => {
   const h = `https://www.songkick.com${href}${queryParams}`;
   try {
     await page.goto(`${h}&page=${pagination}#metro-area-calendar`, { waitUntil: 'networkidle2' });
-    await page.waitForSelector('.microformat', {timeout: 1500 });
+    await page.waitForSelector('.microformat', {timeout: 3000 });
     let events = await page.evaluate(() => {
       if(!document.querySelector(".microformat")){
         return ['undefined'];
